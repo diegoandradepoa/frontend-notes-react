c9cce283638745101c4f1f9f604d81174b5fc275
Diego Andrade <diegoandradecruz15@gmail.com>
2020-07-19 21:15:26 -0300

    frontend

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./styles.css";
 
-const Header = () => <div id="main-header">NOTAS</div>
+const Header = () => <div className="main-header">NOTAS</div>
 
 export default Header;
