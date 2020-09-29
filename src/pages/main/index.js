08b8de1bb607a4b967a07d75c5aa774254c5ac85
Diego Andrade <diegoandradecruz15@gmail.com>
2020-09-28 22:05:04 -0300

    "frontend"

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,37 +1,39 @@
 import React, { Component } from "react";
-import api from '../../services/api'; // Importar a API
-import "./styles.css"
+import api from "../../services/api"; // Importar a API
+import "./styles.css";
 
 export default class main extends Component {
+  state = {
+    //O conceito de Estado é sempre um objeto
+    note: [],
+  };
 
-    state = {        //O conceito de Estado é sempre um objeto
-        note: [],
-    }
+  componentDidMount() {
+    // métodos que pertencem ao react é liberado o uso de funções padrão e não somene arrow funcition
+    this.loadNotes(); // tem que usar função normal para o metodo this referenciar a classe;
+  }
 
-    componentDidMount() {// métodos que pertencem ao react é liberado o uso de funções padrão e não somene arrow funcition
-    this.loadNotes();  // tem que usar função normal para o metodo this referenciar a classe;
-    }
-    
-    loadNotes = async () => { // Arrow functions não sobreescreve o valor do this
-        const response = await api.get('/note');
+  loadNotes = async () => {
+    // Arrow functions não sobreescreve o valor do this
+    const response = await api.get("/");
 
-        this.setState({ note: response.data.docs });
-     };
-    
-    render() {
-        const { note } = this.state;
-        return (
-            <div className="note-list">   
-            {note.map(note =>( //Sempre que se usa o método map dever ser inserido o atributo key com um valor único.
-                <article key={note._id}>
-                    <strong>{note.title}</strong>
-                    <p>{note.description}</p>
-                    <p>{note.createdAt}</p>
-                </article> // 
-            ))}
-            </div>
-        )
-    }
+    this.setState({ note: response.data.docs });
+  };
+
+  render() {
+    const { note } = this.state;
+    return (
+      <div className="note-list">
+        {note.map((
+          note //Sempre que se usa o método map dever ser inserido o atributo key com um valor único.
+        ) => (
+          <article key={note._id}>
+            <strong>{note.title}</strong>
+            <p>{note.description}</p>
+            <p>{note.createdAt}</p>
+          </article> //
+        ))}
+      </div>
+    );
+  }
 }
-
-
